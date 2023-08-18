b809236074cae46503245dd7a3eb18053e2f119c
Ufuk Orhan <ufukorhan@Ufuks-MBP.home>
2023-08-18 18:45:35 +0300

    Implements the Swagger tool for the API documentation

--- core/urls.py
+++ core/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from uavs.views import UAVCategoryViewSet, UAVViewSet, RentedUAVViewSet
 from users.views import UserViewSet
 
@@ -25,6 +29,21 @@ router.register(r"uav-categories", UAVCategoryViewSet)
 router.register(r"uavs", UAVViewSet)
 router.register(r"rented-uavs", RentedUAVViewSet)
 router.register(r"users", UserViewSet)
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -37,4 +56,7 @@ urlpatterns = [
             ]
         ),
     ),
+                path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
